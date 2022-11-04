4db2868e11a7f62e16edac5e882cbcdfe36be695
MomonoKawabata <c0a201372f@edu.teu.ac.jp>
2022-11-04 10:46:35 +0900

    描いた

--- js/main.js
+++ js/main.js
@@ -31,14 +31,85 @@ window.onload = function () {
             }
             //マウス位置を原点として拡大縮小
             canvas.zoomToPoint(new fabric.Point((window.innerWidth / 2), (window.innerHeight / 2)), zoom + deltaY / 2400);
+
+            
     }
 
 
-
+    
     //円
-    canvas.add(new fabric.Circle({
+    var cirle=new fabric.Circle({
         left: (window.innerWidth / 2)-500, top: (window.innerHeight / 2)-500, radius: 500, stroke: 'black',fill: 'lightblue'
-    }));
+    });
 
+    canvas.add(cirle);
+
+    cirle.selectable = false; //地球固定
+    
+
+
+
+
+
+
+
+
+    //島 400 350
+    //土台
+    var island1 = new fabric.Path('M 400 350  L 520 350  C 520 350 453 230 400 350', {stroke: 'brack', fill: '#F5DEB3', strokeWidth: 2}
+    );
+    //上
+    var island2 =new fabric.Path('M 410 328 C 410 328 410 360 435 330 C 435 330 440 360 485 330 C 485 330 495 360 510 330 C 510 330 463 230 410 330', {stroke: 'brack', fill: '#7FFF00', strokeWidth: 2}
+    );
+
+    //木
+    var tree1 = new fabric.Path('M 100 100 C 100 100  80 120 90 150 L 105 150 C 105 150 95 120 108 100 z ', {stroke: 'brack', fill: '#CD853F', strokeWidth: 2 ,top:240,left:450 }
+    );
+    var tree2 = new fabric.Path('M 100 100 C 100 100 130 80 150 115 z M 100 100 C 100 100 70 80 50 115 z M 100 100  ', {stroke: 'brack', fill: 'LimeGreen', strokeWidth: 2 ,left:416,top:234}
+    );
+    var tree3 = new fabric.Path('M 100 100 C 100 100 85 50 50 90 z M 100 100 C 100 100 115 50 150 90 z', {stroke: 'brack', fill: 'Green', strokeWidth: 2 ,left:415,top:215}
+    );
+
+
+    //波
+    var wavebrige1 =new fabric.Path('M 100 100 C 100 100 107 105 110 100 C 110 100 115 93 120 100 C 120 100 125 107 130 100 C 130 100 135 93 140 100', {stroke: '#4169E1', fill:"", strokeWidth: 7, left: 380 ,top:346}
+    );
+    var wavebrige2 =new fabric.Path('M 100 100 C 100 100 107 105 110 100 C 110 100 115 93 120 100 C 120 100 125 107 130 100 C 130 100 135 93 140 100', {stroke: '#4169E1', fill:"", strokeWidth: 7, left: 419 ,top:346}
+    );
+    var wavebrige3 =new fabric.Path('M 100 100 C 100 100 107 105 110 100 C 110 100 115 93 120 100 C 120 100 125 107 130 100 C 130 100 135 93 140 100', {stroke: '#4169E1', fill:"", strokeWidth: 7, left: 458 ,top:346}
+    );
+  
+    
+
+    //橋 503 330
+    var bridge1 = new fabric.Path('M 503 330 L 603 330  620 350  520 350 503 330 M 520 350 L 520 360 620 360 620 350 M 503 330 L 503 340 520 360 520 350 ',{stroke: 'brack', fill: 'Sienna', strokeWidth: 2})
+    
+    var bridge2 = new fabric.Path('M 545 360 L 545 350 528 330 M 570 360 L 570 350 553 330 M 595 360 L 595 350 578 330 ',{stroke: 'brack', fill: '', strokeWidth: 2})
+
+
+    //橋　脚
+    var bridge3 = new fabric.Path('M 531 360 L 531 375 535 375 535 360 M 593 360 L 593 375 598 375 598 360',{stroke: 'brack', fill: 'Sienna', strokeWidth: 2})
+
+    var  bridge = new fabric.Group([bridge1, bridge2 ,bridge3],{left:470});
+
+    //波
+ 
+    var wavebrige4 =new fabric.Path('M 100 100 C 100 100 107 105 110 100 C 110 100 115 93 120 100 C 120 100 125 107 130 100 C 130 100 135 93 140 100', {stroke: '#4169E1', fill:"", strokeWidth: 7, left: 540 ,top:370}
+    );
+    var wavebrige5 =new fabric.Path('M 100 100 C 100 100 107 105 110 100 C 110 100 115 93 120 100 C 120 100 125 107 130 100 C 130 100 135 93 140 100', {stroke: '#4169E1', fill:"", strokeWidth: 7, left: 477 ,top:370}
+    );
+
+
+    var  island = new fabric.Group([tree1,tree2,tree3,island1, island2,wavebrige1,wavebrige2,wavebrige3]);
+    
+    var  ball = new fabric.Group([bridge,wavebrige4,wavebrige5]);
+    canvas.add(island);
+    canvas.add(ball);
+
+    island.selectable = false;
+    ball.selectable = false;
+
+
+  
 
 }
